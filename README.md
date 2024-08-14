92f1554ba56bfdf88e191e7df4b375080e9c4133
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-14 08:00:55 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Ascii art generator
+this is a text art generator that use the pyfiglet library to either output the user's input in a random font or a font chosen by the user. I chose to do this because i always thought ascii art was very cool as a kid and a saw that it was listed as a beginner python project online(apart from getting the library, i did everything myself, dont worry ;) )
+
--- ascii art gen.py
+++ ascii art gen.py
@@ -2,18 +2,21 @@ from pyfiglet import Figlet
 import sys
 import random
 def main():
-    s= input("Input:")
     figlet = Figlet()
     fonts = figlet.getFonts()
     if len(sys.argv) == 1:
         random_font= random.choice(fonts)
         figlet.setFont(font=random_font)
+        s= input("Input:")
     elif len(sys.argv) == 3:
         if sys.argv[1] == "-f" or sys.argv[1] == "--font":
             if sys.argv[2] in fonts:
                 figlet.setFont(font= sys.argv[2])
+                s= input("Input:")
             else:
-                print("Invalid Font")
+                sys.exit("Invalid usage")
+        else:
+            sys.exit("Invalid usage")
 
     print(figlet.renderText(s))
 
