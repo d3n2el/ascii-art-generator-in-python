f7a91432047e54b997b5912851e516b44e5689bc
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-14 07:43:35 +0200

    Add files via upload

--- ascii art gen.py
+++ ascii art gen.py
@@ -5,10 +5,17 @@ def main():
     s= input("Input:")
     figlet = Figlet()
     fonts = figlet.getFonts()
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         random_font= random.choice(fonts)
         figlet.setFont(font=random_font)
-    elif len(sys.argv) == 2:
-        figlet.setfont(font= sys.argv[2])
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            if sys.argv[2] in fonts:
+                figlet.setFont(font= sys.argv[2])
+            else:
+                print("Invalid Font")
 
     print(figlet.renderText(s))
+
+
+main()
